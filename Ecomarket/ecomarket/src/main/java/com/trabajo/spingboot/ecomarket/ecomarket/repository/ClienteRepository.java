638f1d9d0f626466a554760fb0cdc6e75ae47332
mikeduoc <mi.laguela@duocuc.cl>
2025-05-30 02:25:21 -0400

    Delete Ecomarket/ecomarket directory

--- Ecomarket/ecomarket/src/main/java/com/trabajo/spingboot/ecomarket/ecomarket/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trabajo.spingboot.ecomarket.ecomarket.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.trabajo.spingboot.ecomarket.ecomarket.entities.Cliente;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Long>{
-
-}
--- Ecomarket/ecomarket/src/main/java/com/trabajo/spingboot/ecomarket/ecomarket/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trabajo.spingboot.ecomarket.ecomarket.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.trabajo.spingboot.ecomarket.ecomarket.entities.Producto;
-
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
-
-    
-}
